b3a63a01a2e46afb933f1799329c43026cb51bcd
Satoshi972 <ruddy.marieluce@gmail.com>
2017-03-10 13:43:32 -0400

    Correctif mineur
    preparation de la structure de l'index, des lien en entente qui redirige
    sur d'autre page
    modif de la base, ajout de champs dans la table site_info

--- index.php
+++ index.php
@@ -1,11 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>Acceuil</title>
-	<?php require_once 'head.php'; ?>
+	<title>Accueil</title>
+	<?php require_once 'inc/head.php'; ?>
 </head>
 <body>
-	
+	<main class="container-full index-main">
+		<header class="page-header index-header">
+
+			<p>
+				<strong><a href="info_resto.php"><?php echo $info['inf_name'] ?></a></strong>
+			</p>
+
+			<p>
+				<?php echo $info['inf_address'] ?>
+			</p>
+
+			<p>
+				<?php echo $info['inf_number'] ?>
+			</p>
+
+			<p class="text-right">
+				<a href="contact.php">Nous contacter</a>
+			</p>
+
+		</header>
+
+		<section class="text-center">
+			<img class="img-responsive" src="<?php $info['inf_picture'] ?>" alt="Header restaurant">
+		</section>
+
+		<section class="col-xs-12">
+			<legend class="text-center">Les recettes du chefs</legend>
+			<figure class="col-md-4">
+				<img class="img-responsive" src="<?php $recipe ?>" alt="Recette du chef 1">
+				<a href="recipe_details.php?id=<?php $recipe['rcp_id'] ?>">lire la recette</a>
+			</figure>
+
+			<figure class="col-md-4">
+				<img class="img-responsive" src="<?php $recipe ?>" alt="Recette du chef 2">
+				<a href="recipe_details.php?id=<?php $recipe['rcp_id'] ?>">lire la recette</a>
+			</figure>
+
+			<figure class="col-md-4">
+				<img class="img-responsive" src="<?php $recipe ?>" alt="Recette du chef 3">
+				<a href="recipe_details.php?id=<?php $recipe['rcp_id'] ?>">lire la recette</a>
+			</figure>
+
+			<div class="text-center">
+				<a href="recipe_list.php">
+					<button>Découvrir toutes les recettes des chefs</button>
+				</a>
+			</div>
+
+		</section>
+		
+	</main>
+	<?php require_once 'inc/script.php' ?>
 </body>
 </html>
