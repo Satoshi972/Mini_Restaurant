f96600e7a6a74884d6bda6d39761812cf3d23216
Satoshi972 <ruddy.marieluce@gmail.com>
2017-03-10 10:48:40 -0400

    modif du fichier connect

--- inc/connect.php
+++ inc/connect.php
@@ -2,7 +2,7 @@
 //gestion de la conection a la bdd
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=shop;charset=utf8', 'root', '');//Je cible ma base
+    $bdd = new PDO('mysql:host=localhost;dbname=restaurant;charset=utf8', 'root', '');//Je cible ma base
 }
 catch (Exception $e) //s'il y a une erreur alors il l'affiche
 {
