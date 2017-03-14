4f5bec0716c010191c2b1c8315a829403d1ee137
sajitech <techede.sajida@orange.fr>
2017-03-14 14:47:06 -0400

    finalisation footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,19 +2,28 @@
 	<div class="container-full">
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="col-md-6">
+			<p>Plan du site</p>
+				<div class="col-md-4">
 					<ul>
-						<li> <a href="index.php"> Accueil </a> </li>
-						<li> <a href="contact.php"> Nous contacter </a> </li>
+						<li> <a href="index.php">| Accueil</a> </li>
+						<li> <a href="contact.php">| Nous contacter</a> </li>
 					</ul>
 				</div>
 
 
-				<div class="col-md-6">
+				<div class="col-md-4">
 				    <ul>
-					    <li> <a href="about.php"> A propos </a> </li>
-						<li> <a href="search.php"> Rechercher une recette </a> </li>
+					    <li> <a href="about.php">| A propos</a> </li>
+						<li> <a href="search.php">| Rechercher une recette</a> </li>
                     </ul>
+				</div>
+				
+				<div class="col-md-4">
+                    <ul>
+                        <li><i class="fa fa-facebook-square"></i></li>
+                        <li><i class="fa fa-twitter-square"></i> </li>
+                    </ul>
+                </div>
 				</div>
 			</div>
 			<!--/.row--> 
