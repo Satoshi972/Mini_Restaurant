946d3c727c50eae966754c630af87e1c3c236f1f
olivie7 <colombe.oliv@gmail.com>
2017-03-13 23:46:53 -0400

    index
    le css me tue vive PHP

--- index.php
+++ index.php
@@ -32,36 +32,49 @@ else
 	</head>
 
 	<body>
+		<div class=" container-full">
+			<div class="row">
+				<div class="col-xs-12">
+					<header class="page-header index-header">
 
-		<header class="page-header index-header">
-			<h2 class="name">
-				<strong><a href="info_resto.php"><?php echo $info['inf_name']; ?></a></strong>
-			</h2>
+						<div class="col-xs-6">
+							<h2 class="name">
+								<strong><a href="info_resto.php"><?php echo $info['inf_name']; ?></a></strong>
+							</h2>
+							<p class="address">
+								<?php echo $info['inf_address']; ?>
+							</p>
+							<p class="address">
+								<?php echo $info['inf_phone']; ?>
+							</p>
+						</div>
 
-			<p class="address">
-				<?php echo $info['inf_address']; ?>
-			</p>
-			<p class="address">
-				<?php echo $info['inf_phone']; ?>
-			</p>
+						<div class="col-xs-6">
+							<div class="text-right address">
+								<a href="contact.php">Nous contacter</a>
+							</div>
+						</div>
 
-			<p class="text-right address">
-				<a href="contact.php">Nous contacter</a>
-			</p>
-		</header>
+					</header>
+				</div>
+			</div>
+		</div>
 
 		<main> 
 			<div class="container-full index-main">
-				<div class="row col-xs-12">
-					<section id="section1" class="text-center">
-						<img class="img-responsive projector" src="<?php echo $info['inf_picture']; ?>" alt="Header restaurant">
-					</section>
+				<div class="row">
+					<div class="col-xs-12">
+						<section id="section1" class="text-center">
+							<img class="img-responsive projector" src="<?php echo $info['inf_picture']; ?>" alt="Header restaurant">
+						</section>
+					</div>
 				</div>
+			</div>
 
-				<section id="section2">
-				<div class="container">
-				<div class="row col-xs-12">				
-					<div class="well container">
+			<div class="well container">								
+				<div class="row col-xs-12">
+					<section id="section2">
+						<legend class="address"><strong>Faites votre choix</strong></legend>				
 						<?php 
 						foreach ($recipe as $key => $value) :
 						?>
@@ -73,72 +86,19 @@ else
 						endforeach;
 						?>
 						<div class="row col-xs-12">
-						<div class="text-center">
-							<a href="recipe_list.php">
-								<button>Découvrir toutes les recettes des chefs</button>
-							</a>
+							<div class="text-center">
+								<a href="list_recipes.php">
+									<button>Découvrir toutes les recettes des chefs</button>
+								</a>
+							</div>
 						</div>
-					</div>
-					</div>
-					</div>
-					</div>
-				</section>
-					
-
-				
+					</section>
+				</div>
 			</div>
-				</main>
-				<footer class="foot">
-					<div class="row">
-						<div class="col-xs-12">
-							<div class="col-md-6">
-								<h3> Lorem Ipsum </h3>
-								<ul>
-									<li> <a href="#"> Lorem Ipsum </a> </li>
-									<li> <a href="#"> Lorem Ipsum </a> </li>
-									<li> <a href="#"> Lorem Ipsum </a> </li>
-									<li> <a href="#"> Lorem Ipsum </a> </li>
-								</ul>
-							</div>
 
+		</main>
 
-							<div class="col-md-6">
-								<ul>
-									<li>
-										<div class="input-append newsletter-box text-center">
-											<input type="text" class="full text-center" placeholder="Email ">
-											<button class="btn  bg-gray" type="button"> Lorem ipsum <i class="fa fa-long-arrow-right"> </i> </button>
-										</div>
-									</li>
-								</ul>
-								<ul class="social">
-									<li> <a href="#"> <i class=" fa fa-facebook">   </i> </a> </li>
-									<li> <a href="#"> <i class="fa fa-twitter">   </i> </a> </li>
-									<li> <a href="#"> <i class="fa fa-google-plus">   </i> </a> </li>
-									<li> <a href="#"> <i class="fa fa-pinterest">   </i> </a> </li>
-									<li> <a href="#"> <i class="fa fa-youtube">   </i> </a> </li>
-								</ul>
-								<ul class="nav nav-pills payments">
-									<li><i class="fa fa-cc-visa"></i></li>
-									<li><i class="fa fa-cc-mastercard"></i></li>
-									<li><i class="fa fa-cc-amex"></i></li>
-									<li><i class="fa fa-cc-paypal"></i></li>
-								</ul> 
-							</div>
-						</div>
-						<!--/.row--> 
-					</div>
-					<!--/.footer-bottom-->
-					<div class="row">
-						<div class="col-xs-12">
-							<p class="pull-left"> Copyright © Footer 2014. All right reserved. </p>
-						</div>
-					</div>
-					<!--/.container--> 
-					<!--/.footer-->
+		<?php require_once 'inc/script.php'; ?>
 
-				</footer>
-			<?php require_once 'inc/script.php'; 
-			?>
-			</body>
-		</html>
+	</body>
+</html>
